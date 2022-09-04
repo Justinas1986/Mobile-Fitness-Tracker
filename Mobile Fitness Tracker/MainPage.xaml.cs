94348866b946efcf1f57e16a3f969c8937d3a0de
Justinas1986 <justinasstu@gmail.com>
2022-09-04 15:16:40 -0600

    Add project files.

---
+++ Mobile Fitness Tracker/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mobile_Fitness_Tracker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnMyProfile_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnExercise_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnClose_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
